999151250fb5a1dc2ba260df4dd879fe105e7b78
datouch <dollar.touch@gmail.com>
2012-05-19 23:39:51 +0700

    using bootstrap template and add unescape

--- metalreal/__init__.py
+++ metalreal/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from werkzeug.datastructures import ImmutableDict
+
 app = Flask(__name__)
 
 from metalreal.redis_session import RedisSessionInterface
 app.session_interface = RedisSessionInterface()
+
+#app.jinja_options = ImmutableDict(extensions=['jinja2.ext.with_'])
 
 import metalreal.main
--- metalreal/main.py
+++ metalreal/main.py
@@ -1,37 +1,40 @@
 from metalreal import app
 from flask import session, redirect, url_for, render_template, request
+from jinja2 import Markup
+
+unescape=Markup
 
 def require_admin_auth(f):
-    def decorated(*args, **kargs):
-        if 'admin' in session:
-            return f(*args, **kargs)
-        else:
-            return redirect(url_for('admin_login'))
-    decorated.__name__ = f.__name__
-    return decorated
+	def decorated(*args, **kargs):
+		if 'admin' in session:
+			return f(*args, **kargs)
+		else:
+			return redirect(url_for('admin_login'))
+	decorated.__name__ = f.__name__
+	return decorated
 
 @app.route('/admin/')
 @app.route('/admin/chapters/')
 @require_admin_auth
 def admin_index():
-    return render_template('admin/index.html')
+	return render_template('admin/index.html', area='chapter', unescape=unescape )
 
 @app.route('/admin/chapters/new')
 def admin_chapter_new():
-    return render_template('admin/chapters/new.html')
+	return render_template('admin/chapters/new.html', area='chapter/new', unescape=unescape )
 
 @app.route('/admin/login/', methods=['GET', 'POST'])
 def admin_login():
-    if 'admin' in session and session['admin'] == 'admin':
-        return redirect(url_for('admin_index'))
+	if 'admin' in session and session['admin'] == 'admin':
+		return redirect(url_for('admin_index'))
 
-    if request.method == 'POST':
-        if request.form['username'] == 'admin' and request.form['password'] == 'admin':
-            session['admin'] = request.form['username']
-            return redirect(url_for('admin_index'))
-    return render_template('admin/login.html')
+	if request.method == 'POST':
+		if request.form['username'] == 'admin' and request.form['password'] == 'admin':
+			session['admin'] = request.form['username']
+			return redirect(url_for('admin_index'))
+	return render_template('admin/login.html')
 
 @app.route('/admin/logout/')
 def admin_logout():
-    session.pop('admin', None)
-    return redirect(url_for('admin_login'))
+	session.pop('admin', None)
+	return redirect(url_for('admin_login'))
