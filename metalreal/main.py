1cc4161e6df29b4547f76d7ba474468dd07fc072
datouch <dollar.touch@gmail.com>
2012-05-20 04:05:20 +0700

    database connection [chapter create, chapter list]

--- metalreal/main.py
+++ metalreal/main.py
@@ -1,8 +1,31 @@
 from metalreal import app
-from flask import session, redirect, url_for, render_template, request
+from flask import session, redirect, url_for, render_template, request, flash
 from jinja2 import Markup
+from sqlalchemy import create_engine, Table, Column, MetaData, ForeignKey
+from sqlalchemy.schema import CheckConstraint
+from sqlalchemy.sql import select, insert
+from sqlalchemy.dialects.postgresql import \
+    ARRAY, BIGINT, BIT, BOOLEAN, BYTEA, CHAR, CIDR, DATE, \
+    DOUBLE_PRECISION, ENUM, FLOAT, INET, INTEGER, INTERVAL, \
+    MACADDR, NUMERIC, REAL, SMALLINT, TEXT, TIME, TIMESTAMP, \
+    UUID, VARCHAR
 
 unescape=Markup
+
+"""
+	Database preparation
+"""
+engine = create_engine("postgresql+psycopg2://tester:tester@localhost:5432/metalreal_dev", client_encoding='utf8', echo=True)
+metadata = MetaData()
+Chapters = Table('chapters', metadata,
+		Column('chapter_id', VARCHAR(10), primary_key = True),
+		Column('title', VARCHAR(120), nullable=False),
+		Column('content', TEXT, nullable=False),
+		Column('updated_at', TIMESTAMP, default='NOW', onupdate='NOW')
+	)
+metadata.create_all(engine)
+metadata.bind = engine
+
 
 def require_admin_auth(f):
 	def decorated(*args, **kargs):
@@ -17,11 +40,39 @@ def require_admin_auth(f):
 @app.route('/admin/chapters/')
 @require_admin_auth
 def admin_index():
-	return render_template('admin/index.html', area='chapter', unescape=unescape )
+	chapters = Chapters.select(order_by=Chapters.c.chapter_id).execute()
+	return render_template('admin/index.html', chapters=chapters, area='chapter', unescape=unescape )
 
-@app.route('/admin/chapters/new')
+@app.route('/admin/chapters/new', methods=['GET', 'POST'])
+@require_admin_auth
 def admin_chapter_new():
-	return render_template('admin/chapters/new.html', area='chapter/new', unescape=unescape )
+	if request.method == 'GET':
+		return render_template('admin/chapters/new.html', area='chapter/new', unescape=unescape )
+	else:
+		insert_query = Chapters.insert().values(chapter_id=request.form['chapter_id'], 
+																						title=request.form['title'], 
+																						content=request.form['content']
+																						)
+
+		try:
+			if request.form['title'] == '' or request.form['content'] == '':
+				raise
+			insert_query.execute()
+			flash('Chapter was created', 'success')
+			return redirect(url_for('admin_index'))
+
+		except Exception, e:
+			# Add flash by checking all possible cause that raise the exception
+			if request.form['title'] == '':
+				flash("Title can't be blank", 'error')
+
+			if request.form['content'] == '':
+				flash("Content can't be blank", 'error')
+
+			if 'chapters_pkey' in e.message:
+				flash('Chapter number is duplicate', 'error')
+
+			return render_template('admin/chapters/new.html', area='chapter/new', unescape=unescape, chapter=request.form )
 
 @app.route('/admin/login/', methods=['GET', 'POST'])
 def admin_login():
